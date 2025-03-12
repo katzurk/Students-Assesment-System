2e210ca5fc8cbfaeadc7939391eafa12ab748100
Roni <weronika@maslana.com>
2025-03-12 23:00:19 +0100

    added data, logo, updated readme and added small refinedments

--- just-grades-rest/src/main/java/JustGrades/app/controller/UserController.java
+++ just-grades-rest/src/main/java/JustGrades/app/controller/UserController.java
@@ -1,10 +1,8 @@
 package JustGrades.app.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +10,7 @@ import JustGrades.app.model.User;
 import JustGrades.app.model.UserRepository;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
--- just-grades-rest/src/main/resources/scripts/data-h2.sql
+++ just-grades-rest/src/main/resources/scripts/data-h2.sql
@@ -3,4 +3,7 @@ SELECT NEXT VALUE FOR USERS_SEQ;
 INSERT INTO USERS
     (USER_ID, FIRST_NAME, LAST_NAME, PASSWORD_HASH)
 VALUES
-    (1, 'w', 'm', '1');
+    (1, 'w', 'm', '1'),
+    (2, 'Marie', 'Johnson', '123'),
+    (3, 'Alicja', 'Kot', '321'),
+    (4, 'Jan', 'Kowalski', 'qwerty');
