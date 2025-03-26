d2b8e038feac3d19a4d138fda1a8a836d81eb565
Roni <weronika@maslana.com>
2025-03-26 22:31:07 +0100

    expanded schema-h2 and data-h2

--- just-grades-rest/src/main/resources/scripts/data-h2.sql
+++ just-grades-rest/src/main/resources/scripts/data-h2.sql
@@ -1,9 +1,39 @@
-SELECT NEXT VALUE FOR USERS_SEQ;
+-- SELECT NEXT VALUE FOR USERS_SEQ;
 
 INSERT INTO USERS
-    (USER_ID, FIRST_NAME, LAST_NAME, PASSWORD_HASH)
+    (USER_ID, FIRST_NAME, LAST_NAME, PASSWORD_HASH, email)
 VALUES
-    (1, 'w', 'm', '1'),
-    (2, 'Marie', 'Johnson', '123'),
-    (3, 'Alicja', 'Kot', '321'),
-    (4, 'Jan', 'Kowalski', 'qwerty');
+    (1, 'w', 'm', '1', 'w.m@gmail.com'),
+    (2, 'Marie', 'Johnson', '123', 'marie@g.com'),
+    (3, 'Alicja', 'Kot', '321', 'all.k@g.com'),
+    (4, 'Jan', 'Kowalski', 'qwerty', 'king.jan@g.com');
+
+INSERT INTO courses (course_id, name, ects)
+VALUES
+    (1, 'philosophy', 4),
+    (2, 'work ethic', 3),
+    (3, 'lerning methods', 4),
+    (4, 'modern physics', 6),
+    (5, 'basics of physics', 5);
+
+INSERT INTO completion_req (req_id, min_score, type)
+VALUES
+    (1, 51, 'physic'),
+    (2, 31, 'social courses');
+
+INSERT INTO course_req (courses_id, req_id)
+VALUES
+    (1, 2),
+    (2, 2),
+    (3, 2),
+    (4, 1),
+    (3, 1);
+
+INSERT INTO course_reg (reg_id, regulation_text, courses_id)
+VALUES
+    (1, '> 30 ects', 1),
+    (2, 'nothing', 2),
+    (3, 'nothing', 3),
+    (4, '> 61 ects', 4),
+    (5, 'complited basics of physics', 4),
+    (6, 'nothing', 5);
