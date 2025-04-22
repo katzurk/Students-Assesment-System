c8bc5eec6c27e4960de92577847cf372981aaeb0
katzurk <kanickakt@gmail.com>
2025-04-22 20:31:12 +0200

    added redirecting if user not logged in

--- just-grades-ui/app/student-info/page.tsx
+++ just-grades-ui/app/student-info/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import React, {useEffect, useState} from "react";
 import Link from "next/link";
+import styles from './StudentInfo.module.css';
 import {Container, Grid, Button, Box, Typography} from "@mui/material";
 import {StudentCard} from "@/app/student-info/student-card";
+import {CoursesView} from "@/app/student-info/courses-view";
 
 export default function StudentInfo() {
     return (
         <Container maxWidth="xl">
             <Grid container spacing={2}>
-                <Grid size={4}>
+                <Grid size={{xs: 12, md: 4}}>
                     <StudentCard />
-                    <br></br>
-                    <Box display="flex" gap={2}>
+                    <Box display="flex" gap={2} sx={{mt: 2}}>
                         <Link href="student-info/courses">
                             <Button variant="contained">My courses</Button>
                         </Link>
@@ -20,8 +21,11 @@ export default function StudentInfo() {
                         </Link>
                     </Box>
                 </Grid>
-                <Grid size={6}>
-                    <Typography variant="h5">Courses</Typography>
+                <Grid size={{xs: 12, md: 8}}>
+                    <Box className={styles.coursesContainer}>
+                        <Typography variant="h5">Courses</Typography>
+                        <CoursesView/>
+                    </Box>
                 </Grid>
             </Grid>
         </Container>
