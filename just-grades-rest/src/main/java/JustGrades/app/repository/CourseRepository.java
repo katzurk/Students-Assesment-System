93efeebf3199bb5e1dbc18b36308594267a1c1dc
Roni <weronika@maslana.com>
2025-04-23 22:30:00 +0200

    loading courses optimalization

--- just-grades-rest/src/main/java/JustGrades/app/repository/CourseRepository.java
+++ just-grades-rest/src/main/java/JustGrades/app/repository/CourseRepository.java
@@ -4,11 +4,13 @@ import JustGrades.app.model.Course;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends CrudRepository<Course, Long>{
 
+    @EntityGraph(attributePaths = {"completionRequirements", "enrollRequirements"})
     List<Course> findAllByOrderByNameAsc();
 }
