f16ba5f219b84fa66324932b56ded8b3418d3ac3
dianap25 <dianapelin25@gmail.com>
2025-05-05 20:58:04 +0200

    Changed jdbc to jpa for AdminService

--- just-grades-rest/src/main/java/JustGrades/app/repository/UserRepository.java
+++ just-grades-rest/src/main/java/JustGrades/app/repository/UserRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import JustGrades.app.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -17,4 +19,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(@Param("email") String email);
 
     User findUserByUserId(long l);
+
+    @Modifying
+    @Transactional
+    @Query(value = "CALL open_semester()", nativeQuery = true)
+    void openSemester();
+
+    @Modifying
+    @Transactional
+    @Query(value = "CALL close_semester()", nativeQuery = true)
+    void closeSemester();
 }
